e4afe7923280d5f5752306dc339b2ba2ff4b243f
Ben Gordon <benharrisgordon@gmail.com>
2021-03-12 13:37:30 +0100

    Initial set up, before implementing API

---
+++ src/components/app.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: 'xT9IgDEI1iZyb2wqo8'
+    };
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
